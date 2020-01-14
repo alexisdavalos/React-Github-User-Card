8126a99d56f7d94aa903a18485daae5de7264463
alexisdavalos <alexisdavalos.tech@gmail.com>
2020-01-14 21:52:18 +0100

    Added Axios

--- github-users/src/App.js
+++ github-users/src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  //creates constructor function
+  constructor(){
+    super()
+    this.state = {
+      GithubUser: []
+    }
+  }
+  componentDidMount(){
+
+  }
+
+  fetchGithubUser = (user) => {
+    axios.get(`https://api.github.com/users/${user}`)
+
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
